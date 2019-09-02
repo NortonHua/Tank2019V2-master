57e8008ceb8b483068a5abd430cf455a7cfde775
NortonHua <1428370960@qq.com>
2019-09-03 07:40:46 +0800

    第四次修改

--- src/cn/demo/NewAdd.java
+++ src/cn/demo/NewAdd.java
@@ -4,6 +4,7 @@ public class NewAdd {
     public static void main(String[] args) {
         int a=10,b=20;
         int c=a+b;
-        System.out.println(c);
+
+        System.out.println(a+"+"+b+"的和为："+c);
     }
 }

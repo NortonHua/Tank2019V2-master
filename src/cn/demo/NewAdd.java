e044a22c8f434d644f2a717708cab04ff31e20d8
NortonHua <1428370960@qq.com>
2019-09-03 07:34:08 +0800

    第三次修改

--- src/cn/demo/NewAdd.java
+++ src/cn/demo/NewAdd.java
@@ -2,6 +2,8 @@ package cn.demo;
 
 public class NewAdd {
     public static void main(String[] args) {
-        System.out.println("第二次修改项目！");
+        int a=10,b=20;
+        int c=a+b;
+        System.out.println(c);
     }
 }
